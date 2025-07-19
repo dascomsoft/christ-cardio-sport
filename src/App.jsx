6a87c1ab288967d5b55fb869ee3464dd637ba8d5
Dassi Dieudonne <dascomsoft@gmail.com>
2025-07-19 07:26:13 +0100

    contact modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Gallery from "./components/Gallery";
-import Pricing from "./components/Pricing";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar"
-import Programs from "./components/Programs";
-import { Footer } from "./components/Footer";
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./components/About";
+// import Services from "./components/Services";
+// import Gallery from "./components/Gallery";
+// import Pricing from "./components/Pricing";
+// import Contact from "./components/Contact";
+// import Navbar from "./components/Navbar"
+// import Programs from "./components/Programs";
+// import { Footer } from "./components/Footer";
 
 
-const App = () => (
+// const App = () => (
   
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/programs" element={<Programs />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+//       <BrowserRouter>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/services" element={<Services />} />
+//           <Route path="/programs" element={<Programs />} />
+//           <Route path="/gallery" element={<Gallery />} />
+//           <Route path="/pricing" element={<Pricing />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
    
-);
+// );
 
-export default App;
+// export default App;
